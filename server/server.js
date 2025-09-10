7d357c6c8de24f84f9722fc59c0684f83303eefb
Satish Nemali <satishnemali01@gmail.com>
2025-09-10 23:03:03 +0530

    fix cors issuse

--- server/server.js
+++ server/server.js
@@ -13,7 +13,24 @@ const app = express();
 const PORT = process.env.PORT || 5000
 
 connectDB();
-app.use(cors());
+
+const allowedOrigins = [
+    "http://localhost:5174",
+    "https://rbas-system.onrender.com",
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("CORS not allowed"));
+        }
+    },
+    credentials: true
+}));
+
+
 app.use(express.json());
 
 
